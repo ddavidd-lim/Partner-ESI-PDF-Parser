147ba7ac13c2a490abe1c00fa2740b46b842cc9b
Laura Valko <lvalko@uci.edu>
2024-02-13 20:23:11 -0800

    Added get_section function

--- output/transfer_text.py
+++ output/transfer_text.py
@@ -1,7 +1,3 @@
-# reading from output_text.txt
-# find the sections we need to transfer
-# copy the text in the section to the copied word doc
-
 import shutil
 
 def copy_word_document(original_path, copy_path):
@@ -15,8 +11,38 @@ def copy_word_document(original_path, copy_path):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-# Example usage:
 original_document_path = './data/raw/21-342562.35_Training_Report.docx'
 copy_document_path = 'output/21-342562.35_Generated_Report.docx'
-
 copy_word_document(original_document_path, copy_document_path)
+
+
+
+def get_section(start_section, end_section):
+    # TODO: optimize logic to one loop
+    # TODO: track start:stop sections in dict
+    file_path = "output/output_text.txt"
+
+    i = 0
+    sections = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            i += 1
+            if start_section in line:
+                sections.append(i)
+                print(line, i)
+            if end_section in line:
+                sections.append(i)
+        sections = sections[2:]
+        print(sections)
+        print()
+
+    section_lines = ""  
+    with open(file_path, 'r') as file:
+            for i, line in enumerate(file, start=1):
+                if sections[0]+1 < i < sections[1]:
+                    section_lines += line
+    print(section_lines)
+
+start_section = input("Section to transfer: ")
+end_section = input("Next Section: ")
+get_section(start_section, end_section)
