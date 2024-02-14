e8d1c9c4ffe34e2b5813a3180b37c5bc01265b37
Natalie Perrochon <nperroch@uci.edu>
2024-02-13 21:05:06 -0800

    Can write to word file in a given location

--- output/transfer_text.py
+++ output/transfer_text.py
@@ -3,6 +3,13 @@
 # copy the text in the section to the copied word doc
 
 import shutil
+from docx import Document
+from xml.sax.saxutils import escape
+import re
+
+
+
+
 
 def copy_word_document(original_path, copy_path):
     try:
@@ -20,3 +27,72 @@ original_document_path = './data/raw/21-342562.35_Training_Report.docx'
 copy_document_path = 'output/21-342562.35_Generated_Report.docx'
 
 copy_word_document(original_document_path, copy_document_path)
+
+
+# Find line that says 2.4.1
+target_string = '2.4.1'
+stop_string = '2.4.2'
+
+big_string_for_word = ''
+
+file_path = 'output/output_text.txt'
+
+
+with open(file_path, 'r') as file:
+    use_line = False
+    for line in file:
+        if target_string in line:
+            use_line = True
+        if stop_string in line:
+           use_line = False
+        #print(line)
+        if use_line:
+            big_string_for_word += line
+
+# print(big_string_for_word)
+
+
+
+def replace_line_in_docx(file_path, target_string, target_header, stop_string, stop_header, replacement_string):
+    sanitized_text = re.sub(r'[^\x20-\x7E]', '', replacement_string)
+
+    doc = Document(file_path)
+
+    flag = False
+    
+    for i, paragraph in enumerate(doc.paragraphs):
+        # print(paragraph.text)
+        # print()
+        if paragraph.text.strip().split() == [target_string, target_header]:
+            #print(paragraph.text)
+            flag = True
+            #new_paragraph = doc.add_paragraph(replacement_string)
+            doc.paragraphs[i + 1].insert_paragraph_before(replacement_string)
+
+        
+        if paragraph.text.strip().split() == [stop_string, stop_header]:
+            flag = False
+            
+
+
+        if flag:
+            #p = paragraph._element
+            #p.getparent().remove(p)
+            print(paragraph.text)
+        # if target_string in paragraph.text:
+        #         doc.add_paragraph(sanitized_text)
+
+    doc.save(file_path)
+
+# Replace 'your_file.docx' with the actual path to your DOCX file. 
+file_path = 'your_file.docx'
+target_string = '2.4.2'
+target_header = 'Hydrology'
+
+stop_string = '2.4.3'
+stop_header = 'Geology/Soils'
+
+practice_text = "HELLO THIS IS WORKING "
+
+replace_line_in_docx(copy_document_path, target_string, target_header, stop_string, stop_header, big_string_for_word)
+
