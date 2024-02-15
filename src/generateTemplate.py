8852a727cd0961e6ec13d085dc113c8c3f53e949
David Lim <ddavidd.lim@gmail.com>
2024-02-15 12:32:39 -0800

    Modified flagText to take in nlp as argument

--- src/generateTemplate.py
+++ src/generateTemplate.py
@@ -4,6 +4,8 @@ import sys
 sys.path.append('../src/')
 import ReadPDF as rdpdf
 from flagEntities import flagText
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
 
 
 if __name__ == "__main__":
@@ -15,10 +17,15 @@ if __name__ == "__main__":
   output_path = os.path.join(current_directory, "..", "data","processed", "flagged_text.txt")
   out = open(output_path, "w", encoding="utf-8")
   
+  # Load NLP
+  tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+  model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+  nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+  
   for section_num, subsections in subsection_map.items():
     for section in subsections:
       section_text = "\n".join(section.text)
-      text = flagText(section_text)
+      text = flagText(nlp, section_text)
       out.write(f"{section.section_num}\n")
       out.write(f"{section.section_title}\n")
       out.write(f"{text}\n")
