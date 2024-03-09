94bde18da5f46ff42ef940c443923b12eb43131e
David Lim <ddavidd.lim@gmail.com>
2024-03-09 00:26:05 -0800

    Created FieldMapping

---
+++ src/classes/FieldMapping.py
@@ -0,0 +1,12 @@
+class FieldMapping:
+    def __init__(self):
+        self.fields: dict = dict()
+
+    def items(self):
+        return self.fields.items()
+
+    def add_datafield(self, section_num, datafield, value):
+        if section_num in self.fields:
+            self.fields[section_num][datafield] = value
+        else:
+            raise ValueError("Section does not exist.")
