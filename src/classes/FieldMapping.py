19a696817cde2be3f19c4314de7020d22d1f089e
David Lim <ddavidd.lim@gmail.com>
2024-03-09 00:45:18 -0800

    fixed indentation in FieldMapping

--- src/classes/FieldMapping.py
+++ src/classes/FieldMapping.py
@@ -1,12 +1,12 @@
 class FieldMapping:
-    def __init__(self):
-        self.fields: dict = dict()
+	def __init__(self):
+		self.fields = dict()
+  
+	def items(self):
+		return self.fields.items()
 
-    def items(self):
-        return self.fields.items()
-
-    def add_datafield(self, section_num, datafield, value):
-        if section_num in self.fields:
-            self.fields[section_num][datafield] = value
-        else:
-            raise ValueError("Section does not exist.")
+	def add_datafield(self, section_num, datafield, value):
+			if section_num in self.fields:
+					self.fields[section_num][datafield] = value
+			else:
+					raise ValueError("Section does not exist.")
