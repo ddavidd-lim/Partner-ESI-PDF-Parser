c8e8113bd127d1777928b0da87c66f0f8a95b9b9
David Lim <ddavidd.lim@gmail.com>
2024-03-09 00:51:18 -0800

    Added docstrings to prompter and fieldmapping

--- src/classes/FieldMapping.py
+++ src/classes/FieldMapping.py
@@ -12,10 +12,27 @@ class FieldMapping:
 		self.fields = dict()
   
 	def items(self):
+		"""
+        Return a view object that displays a list of a given dictionary's key-value pairs as tuples.
+
+        Returns:
+            dict_items: A view object that displays a list of a given dictionary's key-value pairs as tuples.
+        """
 		return self.fields.items()
 
 	def add_datafield(self, section_num, datafield, value):
-			if section_num in self.fields:
-					self.fields[section_num][datafield] = value
-			else:
-					raise ValueError("Section does not exist.")
+		"""
+        Add a datafield mapping to the specified section number.
+
+        Args:
+            section_num (int): The section number to which the datafield mapping will be added.
+            datafield (str): The datafield key.
+            value (str): The question value associated with the datafield.
+
+        Raises:
+            ValueError: If the specified section number does not exist.
+        """
+		if section_num in self.fields:
+				self.fields[section_num][datafield] = value
+		else:
+				raise ValueError("Section does not exist.")
